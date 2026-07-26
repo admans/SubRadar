98b00096ab55c606c73456956565574565701585
admans <243205964@qq.com>
2026-07-26 10:29:26 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "app.subradar"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1352
-        versionName = "1.3.5.2"
+        versionCode = 2000
+        versionName = "2.0.0.0"
     }
 
     buildFeatures {
