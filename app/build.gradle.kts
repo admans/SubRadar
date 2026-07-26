0db69e2e58632fb534910b6251644f94628b9d29
admans <243205964@qq.com>
2026-07-26 09:12:30 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "app.subradar"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "app.subradar"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1352
+        versionName = "1.3.5.2"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("top.yukonga.miuix.kmp:miuix-ui-android:0.9.3")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+}
